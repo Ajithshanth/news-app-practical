9cea3b032d80f12d159238b766f6a665bec033ac
ajithTrakee <ajithshanth.murugaverl@trakee.com>
2022-08-07 16:03:34 +0530

    back button comp

---
+++ src/components/BackComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="cursor-pointer mb-2" @click="backToPrevious()">
+    <v-icon large color="black darken-4" class="-ml-2">
+      mdi-arrow-left-circle
+    </v-icon>
+    <span> Back </span>
+  </div>
+</template>
+<script>
+export default {
+  name: "BackComponent",
+  methods: {
+    backToPrevious() {
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
