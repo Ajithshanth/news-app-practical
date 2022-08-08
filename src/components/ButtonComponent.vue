ca4edae848e657635a3dffd42d684851b6290848
ajithTrakee <ajithshanth.murugaverl@trakee.com>
2022-08-08 06:02:42 +0530

    btn component

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,18 @@
+<template>
+ 
+    <button
+      
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"
+    >
+      {{ text }}
+    </button>
+   
+</template>
+<script>
+export default {
+  props: {
+    text: {},
+  },
+  
+};
+</script>
