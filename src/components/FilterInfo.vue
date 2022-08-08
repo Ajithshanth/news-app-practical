45c4ca7c39c56035adc1202dffd58b90b8ad7cb2
ajithTrakee <ajithshanth.murugaverl@trakee.com>
2022-08-08 06:02:31 +0530

    filter component created

---
+++ src/components/FilterInfo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex justify-between items-center mt-2">
+    <div class="text-lg text-indigo-600">
+      Headlines Filtered By : {{ searchText }}
+    </div>
+    <div class="text-lg text-indigo-600">
+      Sources Filtered By : {{ selectedSource }}
+    </div>
+    <div>
+      <ButtonComponent v-on:click.native="clearFilter()" text=" Clear Filter" />
+    </div>
+  </div>
+</template>
+<script>
+import ButtonComponent from "./ButtonComponent.vue";
+export default {
+  props: { searchText: {}, selectedSource: {} },
+  components: { ButtonComponent },
+  methods: {
+    clearFilter(){
+         this.$emit("clearFilter");
+    }
+  }
+};
+</script>
